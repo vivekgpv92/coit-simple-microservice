4f91a7480676e96cd640b925e475f94c9a0088fb
vviswanathan <vviswanathan@csod.com>
2023-12-31 08:47:39 +0530

    checkin

--- coit-frontend/src/App.js
+++ coit-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://54.163.98.246:8080/sentiment', {
+        fetch('http://43.205.144.76:8080/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
